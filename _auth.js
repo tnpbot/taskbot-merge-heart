7aa17f057407a24a5a3b737c708b7b28b23ae692
tnpbot <teamnopantsgaming@gmail.com>
2026-06-23 19:59:12 -0400

    Delete _auth.js

--- _auth.js
+++
@@ -1,9 +0,0 @@
-// ========================================
-// oAuth access token and channel info - Required
-// ========================================
-/** @type {AuthConfig} */
-const _authConfig = {
-  twitch_oauth: "4nfgk6qtpuj7s9irb8ppbosiiakecc", 
-  twitch_username: "botnopants",
-  twitch_channel: "teamnopants",
-};
